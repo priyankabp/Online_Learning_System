9fc5660d9d58e784d7edeed7e98886d18078fa0c
Priyanka Phapale <pbphapale@gmail.com>
2017-10-20 15:27:59 -0400

    Change labels

--- home.php
+++ home.php
@@ -60,31 +60,31 @@
           <!--Items Box Open-->
             <div class="row tag-boxes">
 
-              <!--Comment Box Open-->
+              <!--Courses Box Open-->
               <div class="col-md-6 col-lg-3">
                 <div class="panel panel-primary">
                   <div class="panel-heading">
                     <div class="row">
                       <div class="col-xs-3">
-                        <i class="fa fa-comments fa-5x"></i>
+                        <i class="fa fa-folder-open fa-5x"></i>
                       </div>
                       <div class="col-xs-9">
                         <div class="text-right huge">11</div>
-                        <div class="text-right">New Comments</div>
+                        <div class="text-right">Courses</div>
                       </div>
                     </div>
                   </div>
                   <a href="#">
                     <div class="panel-footer">
-                      <span class="pull-left"> View All Comments</span>
+                      <span class="pull-left"> View All Courses</span>
                       <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
                       <div class="clearfix"></div>
                     </div>
                   </a>
                 </div>
-              </div><!--Comment Box Close-->
+              </div><!--Courses Box Close-->
 
-              <!--Post Box Open-->
+              <!--Module Box Open-->
               <div class="col-md-6 col-lg-3">
                 <div class="panel panel-red">
                   <div class="panel-heading">
@@ -94,67 +94,67 @@
                       </div>
                       <div class="col-xs-9">
                         <div class="text-right huge">18</div>
-                        <div class="text-right">All Posts</div>
+                        <div class="text-right">All Modules</div>
                       </div>
                     </div>
                   </div>
                   <a href="#">
                     <div class="panel-footer">
-                      <span class="pull-left"> View All Posts</span>
+                      <span class="pull-left"> View All Modules</span>
                       <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
                       <div class="clearfix"></div>
                     </div>
                   </a>
                 </div>
-              </div><!--Post Box Close-->
+              </div><!--Module Box Close-->
 
-              <!--Users Box Open-->
+              <!--Assessment Box Open-->
               <div class="col-md-6 col-lg-3">
                 <div class="panel panel-yellow">
+                  <div class="panel-heading">
+                    <div class="row">
+                      <div class="col-xs-3">
+                        <i class="fa fa-graduation-cap fa-5x"></i>
+                      </div>
+                      <div class="col-xs-9">
+                        <div class="text-right huge">30</div>
+                        <div class="text-right">All Assessments</div>
+                      </div>
+                    </div>
+                  </div>
+                  <a href="#">
+                    <div class="panel-footer">
+                      <span class="pull-left"> View All Assessments</span>
+                      <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                      <div class="clearfix"></div>
+                    </div>
+                  </a>
+                </div>
+              </div><!--Assessments Box Close-->
+
+              <!--Studnts Box Open-->
+              <div class="col-md-6 col-lg-3">
+                <div class="panel panel-green">
                   <div class="panel-heading">
                     <div class="row">
                       <div class="col-xs-3">
                         <i class="fa fa-users fa-5x"></i>
                       </div>
                       <div class="col-xs-9">
-                        <div class="text-right huge">30</div>
-                        <div class="text-right">All Users</div>
-                      </div>
-                    </div>
-                  </div>
-                  <a href="#">
-                    <div class="panel-footer">
-                      <span class="pull-left"> View All Users</span>
-                      <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
-                      <div class="clearfix"></div>
-                    </div>
-                  </a>
-                </div>
-              </div><!--Users Box Close-->
-
-              <!--Categories Box Open-->
-              <div class="col-md-6 col-lg-3">
-                <div class="panel panel-green">
-                  <div class="panel-heading">
-                    <div class="row">
-                      <div class="col-xs-3">
-                        <i class="fa fa-folder-open fa-5x"></i>
-                      </div>
-                      <div class="col-xs-9">
                         <div class="text-right huge">9</div>
-                        <div class="text-right">All Categories</div>
+                        <div class="text-right">All Students</div>
                       </div>
                     </div>
                   </div>
                   <a href="#">
                     <div class="panel-footer">
-                      <span class="pull-left"> View All Categories</span>
+                      <span class="pull-left"> View All Students</span>
                       <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
                       <div class="clearfix"></div>
                     </div>
                   </a>
                 </div>
-              </div><!--Categories Box Close-->
+              </div><!--Students Box Close-->
             </div><hr><!-- Items Box Close -->
 
           </div>
